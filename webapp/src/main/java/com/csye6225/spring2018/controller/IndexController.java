9bc0f7c0c59806e0a118790fc212a230ddcceb10
Unknown <dantala.a@northeastern.edu>
2018-01-26 15:33:11 -0800

    AK:"Code Updated"

--- webapp/src/main/java/com/csye6225/spring2018/controller/IndexController.java
+++ webapp/src/main/java/com/csye6225/spring2018/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.hibernate.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,12 +30,8 @@ public class IndexController {
     return "index";
   }
 
-  @Autowired
-  private UserRepository userRepository;
-
   @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
   @ResponseBody
-  @ResponseStatus(HttpStatus.OK)
   public String CreateAccount(@RequestParam("email") String emailAddress, @RequestParam("pass") String password) {
 
     String userId = "";
