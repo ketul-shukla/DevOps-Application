830eaf5a9508ca0d688c6c44846d23f461d7affc
Unknown <shukla.ke@northeastern.edu>
2018-01-29 09:59:12 -0500

    KS: Password Verification code

--- webapp/src/main/java/com/csye6225/spring2018/user/UserRepository.java
+++ webapp/src/main/java/com/csye6225/spring2018/user/UserRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmailID(String emailID);
-    User findByEmailIDAndPassword(String emailID, String password);
 }
