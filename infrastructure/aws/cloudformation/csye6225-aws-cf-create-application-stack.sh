30bb16a72d0867d0bace5f243ac76048661c841c
Unknown <shukla.ke@northeastern.edu>
2018-02-28 01:03:54 -0500

    KS: DBSecurity Issue

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -12,13 +12,15 @@ vpcId=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=$resourceStackName-
 
 securityGroupId=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$vpcId Name=description,Values=csye6225-webapp --query 'SecurityGroups[].GroupId' --output text)
 
+dbSecurityGroupId=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$vpcId Name=description,Values=csye6225-rds --query 'SecurityGroups[].GroupId' --output text)
+
 subnetId=$(aws ec2 describe-subnets --filters Name=tag:Name,Values="$resourceStackName-csye6225-subnet-for-webservers" --query 'Subnets[].SubnetId' --output text)
 
 subnetGroup=$(aws rds describe-db-subnet-groups --filters Name=vpc-id,Values=$VpcId --query 'DBSubnetGroups[].DBSubnetGroupName' --output text)
 
 iamInstanceProfile=$(aws iam list-instance-profiles --query 'InstanceProfiles[].InstanceProfileName' --output text)
 
-aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-application.json --parameters ParameterKey=imgID,ParameterValue=$imageId ParameterKey=instanceType,ParameterValue=$instanceType ParameterKey=volumeType,ParameterValue="gp2" ParameterKey=volumeSize,ParameterValue="16" ParameterKey=keyName,ParameterValue="csye6225" ParameterKey=EC2InstanceName,ParameterValue="csye6225-EC2Instance" ParameterKey=securityGroupId,ParameterValue=$securityGroupId ParameterKey=subnetId,ParameterValue=$subnetId ParameterKey=IamInstanceProfileName,ParameterValue=$iamInstanceProfile ParameterKey=DynamoDBTableName,ParameterValue="csye6225" ParameterKey=S3BucketName,ParameterValue="web-app.csye6225-spring2018-shuklake.me" ParameterKey=DBEngine,ParameterValue="MySQL" ParameterKey=DBEngineVersion,ParameterValue="5.6.37" ParameterKey=DBInstanceClass,ParameterValue="db.t2.medium" ParameterKey=DBInstanceIdentifier,ParameterValue="csye6225-spring2018" ParameterKey=DBUser,ParameterValue="csye6225master" ParameterKey=DBPassword,ParameterValue="csye6225password" ParameterKey=DBSubnetGroup,ParameterValue=$subnetGroup ParameterKey=DBName,ParameterValue="csye6225"
+aws cloudformation create-stack --stack-name $stackName --template-body file://csye6225-cf-application.json --parameters ParameterKey=imgID,ParameterValue=$imageId ParameterKey=instanceType,ParameterValue=$instanceType ParameterKey=volumeType,ParameterValue="gp2" ParameterKey=volumeSize,ParameterValue="16" ParameterKey=keyName,ParameterValue="csye6225" ParameterKey=EC2InstanceName,ParameterValue="csye6225-EC2Instance" ParameterKey=securityGroupId,ParameterValue=$securityGroupId ParameterKey=subnetId,ParameterValue=$subnetId ParameterKey=IamInstanceProfileName,ParameterValue=$iamInstanceProfile ParameterKey=DynamoDBTableName,ParameterValue="csye6225" ParameterKey=S3BucketName,ParameterValue="web-app.csye6225-spring2018-shuklake.me" ParameterKey=DBEngine,ParameterValue="MySQL" ParameterKey=DBEngineVersion,ParameterValue="5.6.37" ParameterKey=DBInstanceClass,ParameterValue="db.t2.medium" ParameterKey=DBInstanceIdentifier,ParameterValue="csye6225-spring2018" ParameterKey=DBUser,ParameterValue="csye6225master" ParameterKey=DBPassword,ParameterValue="csye6225password" ParameterKey=DBSubnetGroup,ParameterValue=$subnetGroup ParameterKey=DBName,ParameterValue="csye6225" ParameterKey=DBSecurityGroupId,ParameterValue=$dbSecurityGroupId
 
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
